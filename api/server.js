296a98235a2c2c03de1ad888a63182d977bc0fc7
OrangeBirdDrinksWater <114991717+OrangeBirdDrinksWater@users.noreply.github.com>
2023-11-30 13:11:40 +0800

    Update server.js

--- api/server.js
+++ api/server.js
@@ -6,6 +6,10 @@ const app = express();
 const port = 5100;
 app.use(express.json());
 
+app.post("/register",async (req,res)=>{
+  await prisma.user.create({name:"john",age:"22"})
+})
+
 app.listen(port, () => {
   console.log(`Server has started on port ${port}.`);
 });
